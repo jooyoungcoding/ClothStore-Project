d3b599cec9ea24ed6005d33dfec5babaad93c9aa
jooyoung <lamtt231024@gmail.com>
2026-02-01 02:03:08 +0700

    done herosection

---
+++ Frontend/my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "@/components/home/HeroSection";
+
+const page = () => {
+  return (
+    <div className="bg-[#ececec]">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default page;
